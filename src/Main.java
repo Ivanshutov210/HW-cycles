a6e97010f2e967ceca2772d682b281bcebe07bfc
Иван <ivan.shutov95@mail.ru>
2025-07-23 22:22:05 +0300

    Hw 7

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,9 @@ public class Main {
         for (int i = 0; i <= 100; i = i + 7) {
             System.out.println(i);
         }
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 }
